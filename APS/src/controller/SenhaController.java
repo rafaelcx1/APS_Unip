b3949366ff391ba35afcacb5650c87a6878834d6
Rafaelcx <rafaelfelipemoraes@yahoo.com.br>
2016-10-21 19:33:52 -0200

    Teste

--- APS/src/controller/SenhaController.java
+++ APS/src/controller/SenhaController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import model.SenhaModel;
 import view.SenhaView;
