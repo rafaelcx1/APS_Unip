8e0a245b9baecaaa5b9e33f67add0eea528f3b71
Rafaelcx <rafaelfelipemoraes@yahoo.com.br>
2016-10-25 16:32:42 -0200

    DAOForum-> Mais alguns métodos feitos

---
+++ APS/src/model/models/UsuarioAtivoModel.java
@@ -0,0 +1,22 @@
+package model.models;
+
+public class UsuarioAtivoModel {
+
+	private String nome;
+	private int numPublicacoes;
+
+
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getNumPublicacoes() {
+		return numPublicacoes;
+	}
+	public void setNumPublicacoes(int numPublicacoes) {
+		this.numPublicacoes = numPublicacoes;
+	}
+
+}
