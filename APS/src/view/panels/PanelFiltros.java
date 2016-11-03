d950f5974b8dcab741edab47a75ee805d79920c1
rafaelcx1 <rafaelfelipemoraes@yahoo.com.br>
2016-11-03 09:58:40 -0200

    Inserido métodos auxiliares no PanelFiltros
    Métodos para deixar o lbl de filtrar data sublinhado e com o hand cursor

--- APS/src/view/panels/PanelFiltros.java
+++ APS/src/view/panels/PanelFiltros.java
@@ -1,5 +1,21 @@
 package view.panels;
 
-public class PanelFiltros {
+public class PanelFiltros extends JPanel {
+    private JLabel lblFiltrarData;
+  
+    public void lblFiltrarDataEntered() {
+      @SuppressWarnings("unchecked")
+			Map<TextAttribute, Integer> atributos = (Map<TextAttribute, Integer>) lblFiltrarData.getFont().getAttributes();
+			atributos.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+			lblFiltrarData.setFont(new Font(atributos));
+			lblFiltrarData.setCursor(new Cursor(Cursor.HAND_CURSOR));
+    }
 
+    public void lblFiltrarDataExited() {
+      @SuppressWarnings("unchecked")
+			Map<TextAttribute, Integer> atributos = (Map<TextAttribute, Integer>) lblFiltrarData.getFont().getAttributes();
+			atributos.put(TextAttribute.UNDERLINE, -1);
+			lblFiltrarData.setFont(new Font(atributos));
+			lblFiltrarData.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+    }
 }
