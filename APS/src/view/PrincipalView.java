96de33049a0ea7da7f939e0931fbfbcce0e9c61a
rafaelcx1 <rafaelfelipemoraes@yahoo.com.br>
2016-11-04 10:25:43 -0200

    Feito os métodos de setListener

--- APS/src/view/PrincipalView.java
+++ APS/src/view/PrincipalView.java
@@ -50,88 +50,96 @@ public class PrincipalView extends JFrame implements IView{
 		return responderTopicoPanel;
 	}
 
-	public void setBarraVerticalDados(String[] usuariosMaisAtivos, String[] tagsMaisAtivos, String[] topicosMaisCurtidos) {
-
+	public void setBarraVerticalDados(String[] usuariosMaisAtivos, String[] tagsMaisAtivas, String[] topicosMaisCurtidos) {
+		barraVertical.setUsuariosMaisAtivos(usuariosMaisAtivos);
+		barraVertical.setTagsMaisAtivas(tagsMaisAtivas);
+		barraVertical.setTopicosMaisCurtidos(topicosMaisCurtidos);
 	}
 
 	public void setTopicos(TopicoModel[] topicos) {
-
+		principalForumPanel.setTopicos(topicos);
 	}
 
 	public void abrirPerfilPanel(UsuarioModel usuario) {
-
+		
 	}
 
 	public void abrirPrincipalForumPanel() {
-
+		
 	}
 
 	public void abrirCriarTopicoPanel(String[] tags) {
-
+		
 	}
 
 	public void abrirVisualizarTopicoPanel(PostagemModel[] posts) {
-
+		
 	}
 
 	public void setBtnInicioListener(ActionListener event) {
-
-	}
-
-	public void setBtnCriarTopicoListener(ActionListener event) {
-
+		barraHorizontal.getBtnInicio().addActionListener(event);
 	}
 
 	public void setBtnMeuPerfilListener(ActionListener event) {
-
+		barraHorizontal.getBtnPerfil().addActionListener(event);
 	}
 
 	public void setBtnMinhasPostagensListener(ActionListener event) {
-
+		barraHorizontal.getBtnPostagens().addActionListener(event);
 	}
 
 	public void setBtnSairListener(ActionListener event) {
-
+		barraHorizontal.getBtnSair().addActionListener(event);
 	}
 
 	public void setBtnNextListener(ActionListener event) {
-
+		principalForumPanel.getBtnNext().addActionListener(event);
 	}
 
 	public void setBtnPreviousListener(ActionListener event) {
-
+		principalForumPanel.getBtnPrevious().addActionListener(event);
 	}
 
 	public void setBtnFiltrarListener(ActionListener event) {
-
+		principalForumPanel.getPanelFiltros().getBtnFiltrar().addActionListener(event);
+	}
+	
+	public void setFiltrarDataTopicosListener(MouseListener event) {
+		principalForumPanel.getPanelFiltros().getLblFiltrarData().addMouseListener(event);
+	}
+	
+	public void setBtnCriarTopicoListener(ActionListener event) {
+		principalForumPanel.getPanelFiltros().getBtnCriarTopico().addActionListener(event);
 	}
 
 	public void setBtnCurtirListener(ActionListener event) {
-
-	}
-
-	public void setBtnPostarRespostaListener(ActionListener event) {
-
+		for(int contador = 0; contador < 5; contador++) {
+			principalForumPanel.getBtnCurtir(contador).addActionListener(event);
+		}
 	}
 
 	public void setBtnVisualizarListener(ActionListener event) {
-
+		for(int contador = 0; contador < 5; contador++) {
+			principalForumPanel.getBtnCurtir(contador).addActionListener(event);
+		}
+	}
+	
+	public void setBtnResponderTopicoListener(ActionListener event) {
+		for(int contador = 0; contador < 5; contador++) {
+			principalForumPanel.getBtnResponderTopico(contador).addActionListener(event);
+		}
+	}
+	
+	public void setBtnPostarRespostaListener(ActionListener event) {
+		responderTopicoPanel.getBtnPostarResposta().addActionListener(event);
 	}
 
 	public void setBtnPostarTopicoListener(ActionListener event) {
-
-	}
-
-	public void setBtnVoltarCriarTopicoListener(ActionListener event) {
-
+		criarTopicoPanel.getBtnPostarTopico().addActionListener(event);
 	}
 
 	public void setBtnSalvarPerfilListener(ActionListener event) {
-
-	}
-
-	public void setFiltrarDataTopicosListener(MouseListener event) {
-
+		perfilPanel.getBtnSalvarPerfil().addActionListener(event);
 	}
 
 	@Override
