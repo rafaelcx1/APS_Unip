580b9e5b8ea78440d2eaa2f200be6131dc7411e7
gabrielcursino <gabriel@fuston.com.br>
2016-11-06 15:22:58 -0200

    Panel de postagem

--- APS/src/view/panels/PanelPostagem.java
+++ APS/src/view/panels/PanelPostagem.java
@@ -1,5 +1,22 @@
 package view.panels;
 
-public class PanelPostagem {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import model.tables.PostagemModel;
 
+public class PanelPostagem extends JPanel {
+
+	private JLabel lblAvatar;
+	private JLabel lblNome;
+	private JLabel lblEstadoCidade;
+	private JLabel lblQtdPostagens;
+	private JLabel lblDataCadastro;
+	private JLabel lblDataResposta;
+	private JLabel lblTextoPostagem;
+	private PostagemModel postagem;
+	
+	public PanelPostagem(PostagemModel postagem){
+		
+	}
+	
 }
