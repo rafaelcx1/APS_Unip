87bdd915f2d9777146f428242ffa1186b2f15b40
rafaelcx1 <rafaelfelipemoraes@yahoo.com.br>
2016-11-07 11:32:07 -0200

    Esqueleto feito

--- APS/src/view/panels/PanelPostagem.java
+++ APS/src/view/panels/PanelPostagem.java
@@ -1,5 +1,16 @@
 package view.panels;
 
-public class PanelPostagem {
-
+public class PanelPostagem extends JPanel{
+	private JLabel lblAvatar;
+	private JLabel lblNome;
+	private JLabel lblEstadoCidade;
+	private JLabel lblDataCadastro;
+	private JLabel lblDataResposta;
+	private JLabel lblTextoPostagem;
+	private PostagemModel postagem;
+	
+	public PanelPostagem(PostagemModel postagem) {
+		this.postagem = postagem;
+		//Continuar
+	}
 }
